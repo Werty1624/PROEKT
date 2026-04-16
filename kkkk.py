0fe07e85d0e44a3c6028b369cae7c87075bb6325
Werty1624 <qwertieslolrt@gmail.com>
2026-04-16 09:16:48 +0500

    Add files via upload

--- kkkk.py
+++ kkkk.py
@@ -9,15 +9,26 @@ label_winner = QLabel("Winner")
 label_number = QLabel('?')
 button_winner = QPushButton('Generate')
 layout = QVBoxLayout()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-layout.addWidget(label_winner)
+layout.addWidget(label_winner)                                                                                          
 layout.addWidget(label_number)
 layout.addWidget(button_winner)
 
 def generate_winner():
-    number = random.randint(1, 10)
+    number = random.randint(1, 100)
     label_number.setText(str(number))
+    if number % 2 == 0:
+        label_winner.setText('Машина')
+    elif number % 3 == 0:
+        label_winner.setText("house")
+    else:
+        label_winner.setText('Вы проиграли')
+
 
 button_winner.clicked.connect(generate_winner)
 window.setLayout(layout)
 window.show()
 app.exec()
+
+
+
+
